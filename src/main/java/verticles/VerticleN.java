1369dc237e006f40eb31b52aaa1c83c47796e005
Jacob Smith <jacobsmithevans@gmail.com>
2022-12-07 02:15:30 -0600

    added config

---
+++ src/main/java/verticles/VerticleN.java
@@ -0,0 +1,13 @@
+package verticles;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+
+public class VerticleN extends AbstractVerticle {
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    System.out.println("Start " + getClass().getName() + " with config " +
+      config().toString());
+    startPromise.complete();
+  }
+}
