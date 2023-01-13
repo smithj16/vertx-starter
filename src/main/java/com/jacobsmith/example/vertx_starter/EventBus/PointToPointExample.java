77288620d31daf62c396098e2d2206d7c2a10912
Jacob Smith <jacobsmithevans@gmail.com>
2023-01-13 17:24:54 -0600

    added PointToPointExample.java and PublishSubscribeExample.java

--- src/main/java/com/jacobsmith/example/vertx_starter/EventBus/PointToPointExample.java
+++ src/main/java/com/jacobsmith/example/vertx_starter/EventBus/PointToPointExample.java
@@ -1,40 +1,4 @@
 package com.jacobsmith.example.vertx_starter.EventBus;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Promise;
-import io.vertx.core.Vertx;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class PointToPointExample {
-
-  public static void main(String[] args) {
-    Vertx vertx = Vertx.vertx();
-    vertx.deployVerticle(new Receiver());
-    vertx.deployVerticle(new Sender());
-  }
-
-  static class Sender extends AbstractVerticle {
-
-    @Override
-    public void start(Promise<Void> startPromise) throws Exception {
-      startPromise.complete();
-      vertx.setPeriodic(1000, id -> {
-        vertx.eventBus().send(Sender.class.getName(), "Sending a message...");
-      });
-    }
-  }
-
-  static class Receiver extends AbstractVerticle {
-
-    private static final Logger log = LoggerFactory.getLogger(Receiver.class);
-
-    @Override
-    public void start(Promise<Void> startPromise) throws Exception {
-      startPromise.complete();
-      vertx.eventBus().<String>consumer(Sender.class.getName(), message -> {
-        log.debug("Message received: " + message.body());
-      });
-    }
-  }
 }
---
+++ src/main/java/com/jacobsmith/example/vertx_starter/EventBus/PublishSubscribeExample.java
@@ -0,0 +1,54 @@
+package com.jacobsmith.example.vertx_starter.EventBus;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+
+public class PublishSubscribeExample {
+  public static void main(String[] args) {
+    Vertx vert = Vertx.vertx();
+    vert.deployVerticle(new Publisher());
+    vert.deployVerticle(new Subscriber1());
+    vert.deployVerticle(new Subscriber2());
+    //vert.deployVerticle(new Publisher());
+  }
+
+  public static class Publisher extends AbstractVerticle {
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+      startPromise.complete();
+      vertx.setPeriodic(Duration.ofSeconds(10).toMillis(), id -> {
+        vertx.eventBus().publish(Publisher.class.getName(), "A message for everyone");
+      });
+    }
+  }
+
+  public static class Subscriber1 extends AbstractVerticle {
+    private static final Logger log = LoggerFactory.getLogger(Subscriber1.class);
+
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+      startPromise.complete();
+      vertx.eventBus().<String>consumer(Publisher.class.getName(), message -> {
+        log.debug("Message recieved by sub1: " + message.body());
+      });
+    }
+  }
+
+  static class Subscriber2 extends AbstractVerticle {
+    private static final Logger log = LoggerFactory.getLogger(Subscriber2.class);
+
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+      startPromise.complete();
+      vertx.eventBus().<String>consumer(Publisher.class.getName(), message -> {
+        log.debug("Message recieved by Sub2: " + message.body());
+      });
+    }
+  }
+
+}
