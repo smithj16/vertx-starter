ce5fbfe1def1c7b5ab03e949d093afe468f13ad3
Jacob Smith <jacobsmithevans@gmail.com>
2023-01-15 00:33:04 -0600

    added Ping.java, Pong.java, PingPongExample.java;

--- src/test/java/com/jacobsmith/example/vertx_starter/FuturePromiseExample.java
+++ src/test/java/com/jacobsmith/example/vertx_starter/FuturePromiseExample.java
@@ -1,5 +1,6 @@
 package com.jacobsmith.example.vertx_starter;
 
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.junit5.VertxExtension;
@@ -18,5 +19,63 @@ public class FuturePromiseExample {
   void promise_success(Vertx vert, VertxTestContext context) {
     final Promise<String> promise = Promise.promise();
     log.debug("start");
+    vert.setTimer(500, id ->{
+      promise.complete("success");
+      log.debug("Promise completed");
+      context.completeNow();
+    });
+    log.debug("End");
   }
+
+  @Test
+  void promise_failure (Vertx vert, VertxTestContext context) {
+    final Promise<String> promise = Promise.promise();
+    log.debug("Start");
+    vert.setTimer(500, id -> {
+      promise.fail("Failure");
+      log.debug("Promise failed");
+      context.completeNow();
+    });
+    log.debug("End");
+
+  }
+
+  @Test
+  void future_success (Vertx vert, VertxTestContext context){
+
+    final Promise<String> promise = Promise.promise();
+    log.debug("Start");
+    vert.setTimer(500, id -> {
+      promise.complete("complete");
+      log.debug("Timer done.");
+      context.completeNow();
+    });
+
+    final Future<String> future = promise.future();
+    future.onSuccess(result -> {
+      log.debug("Result: {}", result);
+      context.completeNow();
+    }).onFailure(context::failNow);
+
+  }
+
+
+  @Test
+  void future_failure(Vertx vertx, VertxTestContext context){
+
+    final Promise<String> promise = Promise.promise();
+    log.debug("Start");
+    vertx.setTimer(500, id -> {
+      promise.fail("failed");
+      context.completeNow();
+    });
+    log.debug("End");
+
+    final Future<String> future = promise.future();
+    future.onFailure(result -> {
+      log.debug("Promised Failed with error: {}", result);
+      context.completeNow();
+    });
+  }
+
 }
